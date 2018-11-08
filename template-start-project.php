4a189f931d757c384d543439f2e9cae7f7f79260
Ben Willman <benjamin.willman@gsa.gov>
2018-11-07 22:55:07 -0500

    added homepage pictures

---
+++ template-start-project.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Template Name: Start Your Project
+ * Template Post Type: page, post
+
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+
+<div>test</div>
