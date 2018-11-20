be07f1f7b477186c03bc243f6bef4f7afa14d4da
Ben Willman <benjamin.willman@gsa.gov>
2018-11-19 23:46:36 -0500

    updated sign-up form

--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
 <?php
 function my_theme_enqueue_styles() {
 
+	remove_filter( 'the_content', 'wpautop' );// added by BW - remove <P> auto add
+	remove_filter( 'the_excerpt', 'wpautop' );
+
     $parent_style = 'twentyseventeen-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
@@ -9,8 +12,13 @@ function my_theme_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+
+
 }
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
+
 
 ?>
