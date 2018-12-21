ffaff27e1cbee9358c57c9f56104042e8116fe64
Ben Willman <benjamin.willman@gsa.gov>
2018-12-21 14:59:34 -0500

    home page panel responsive updates

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ function my_theme_enqueue_styles() {
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
+        get_stylesheet_directory_uri() . '/assets/css/main.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
