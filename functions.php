f95b7bdf57e1e674b532a8ba67de86f89721f590
Ben Willman <benjamin.willman@gsa.gov>
2019-01-08 10:06:59 -0500

    added style.css back into child theme * in functions.php - added code to allow CF7 to work with columns plugin * style.css updates for submit a bid

--- functions.php
+++ functions.php
@@ -8,17 +8,24 @@ function my_theme_enqueue_styles() {
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/assets/css/main.css',
+        get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+
+
 
 
 }
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
-
+add_filter( 'wpcf7_form_elements', 'do_shortcode' );//added BW 1-2-19, allow other shortcodes inside of contact form 7
 
 
 ?>
+
+
+
+
+
